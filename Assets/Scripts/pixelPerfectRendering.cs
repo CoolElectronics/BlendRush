fcc7a34fd80c42317a619e0fc0b3279de5affd69
CoolElectronics <58010778+CoolElectronics@users.noreply.github.com>
2020-07-25 17:20:07 -0400

    movement patch 01

--- Assets/Scripts/pixelPerfectRendering.cs
+++ Assets/Scripts/pixelPerfectRendering.cs
@@ -1,29 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//[ExecuteInEditMode]
+[ExecuteInEditMode]
 public class pixelPerfectRendering : MonoBehaviour
 {
     SpriteRenderer[] sprites;
     Vector3[] spritePos;
+    private void Start() {
+        
+    }
     void OnPreRender()
     {
-        sprites = GameObject.FindObjectsOfType<SpriteRenderer>();
-        spritePos = new Vector3[sprites.Length];
-        for (int i = 0; i < sprites.Length; i++)
+        if (this.enabled)
         {
-            Transform spriteT = sprites[i].gameObject.transform;
-            spritePos[i] = spriteT.position;
-            spriteT.position = new Vector3(spriteT.position.x, Mathf.Round(spriteT.position.y * 2) / 2, 0);
+            sprites = GameObject.FindObjectsOfType<SpriteRenderer>();
+            spritePos = new Vector3[sprites.Length];
+            for (int i = 0; i < sprites.Length; i++)
+            {
+                Transform spriteT = sprites[i].gameObject.transform;
+                spritePos[i] = spriteT.position;
+                spriteT.position = new Vector3(spriteT.position.x, Mathf.Round(spriteT.position.y * 2) / 2, 0);
+            }
         }
     }
 
     void OnPostRender()
     {
-        for (int i = 0; i < sprites.Length; i++)
+        if (this.enabled)
         {
-            Transform spriteT = sprites[i].gameObject.transform;
-            spriteT.position = spritePos[i];
+            for (int i = 0; i < sprites.Length; i++)
+            {
+                Transform spriteT = sprites[i].gameObject.transform;
+                spriteT.position = spritePos[i];
+            }
         }
     }
 }
