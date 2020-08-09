78a54bbb8075da1cdaf3984f7ae4af9cef5bf4af
CoolElectronics <58010778+CoolElectronics@users.noreply.github.com>
2020-08-09 19:58:49 -0400

    Nothing much

--- Assets/Scripts/damageSystem.cs
+++ Assets/Scripts/damageSystem.cs
@@ -8,15 +8,20 @@ public class damageSystem : MonoBehaviour
     public float health = 100;
     public Image image;
     public GameObject hitParticles;
+    public float maxInvulnerableTime;
+    float invulnerableTime;
+    public GameObject shield;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        image.fillAmount = health / 100;
+      shield.SetActive(invulnerableTime > 0);
+      invulnerableTime -= Time.deltaTime;
+        image.fillAmount = health / 10;
             if (health <= 0){
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
@@ -24,20 +29,26 @@ public class damageSystem : MonoBehaviour
     void OnTriggerEnter2D(Collider2D col){
         if (col.gameObject.tag == "Explosion"){
             Destroy(col.gameObject);
-            health -= 10;
-            shake.e.Shake(0.1f,0.1f);
-            Instantiate(hitParticles,transform.position,Quaternion.identity);
+            Damage(1);
         }
     }
     void OnCollisionEnter2D(Collision2D col){
         if (col.gameObject.tag == "Laser"){
-            health = 0;
+            Damage(1);
         }
         if (col.gameObject.tag == "Bullet"){
             Destroy(col.gameObject);
-            health -= 10;
-            shake.e.Shake(0.1f,0.1f);
-            Instantiate(hitParticles,transform.position,Quaternion.identity);
+            Damage(1);
         }
+    }
+    void Damage(int amount){
+      if (invulnerableTime < 0){
+        health -= amount;
+        invulnerableTime = maxInvulnerableTime;
+        shake.e.Shake(1f,0.3f);
+        GameObject s = Instantiate(soundInstance);
+        AudioSource au = s.GetComponent<AudioSource>();
+        au.
+      }
     }
 }
